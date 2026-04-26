13aad727e8df38c4ab30afd153e4aaa69bfb404c
Ray Morganti <97129173+RayMorganti@users.noreply.github.com>
2026-04-26 03:15:35 -0400

    Update headlight_right.py

--- examples/headlight_right.py
+++ examples/headlight_right.py
@@ -10,9 +10,9 @@ from time import sleep
 robot = CutebotPro()
 
 try:  # Start a try block for basic error handling.
-    robot.set_headlights(LightSelector.RIGHT, 0, 0, 255)  # Turn on both headlights and set them to green.
+    robot.set_headlights(LightSelector.RIGHT, 0, 0, 255)  # Turn on the right headlight and set it to blue.
     sleep(10)  # Keep both headlights green for 10 seconds.
-    robot.set_headlights(LightSelector.RIGHT, 0, 0, 0)  # Turn off all headlights.
+    robot.set_headlights(LightSelector.RIGHT, 0, 0, 0)  # Turn off the headlight.
 except Exception as error:  # Catch any runtime error.
     print("Headlight demo 1 error:", error)  # Print the error message.
-    robot.set_headlights(LightSelector.RIGHT, 0, 0, 0)  # Turn off all headlights for safety.
+    robot.set_headlights(LightSelector.RIGHT, 0, 0, 0)  # Turn off the headlight for safety.
